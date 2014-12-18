32140c915b88e1e1e1fd1b0e24890aab19321d9b
Chelsea Franz <chelseafranz3@gmail.com>
2014-12-18 14:06:20 -0500

    added gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 'use strict';
-// generated on 2014-12-16 using generator-tiy-webapp 0.0.8
+// generated on 2014-12-18 using generator-tiy-webapp 0.0.8
 
 // Require your modules
 var gulp = require('gulp');
